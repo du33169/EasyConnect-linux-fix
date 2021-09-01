fe7436750c4080f2b3bf03b615d131e12f1c9784
du33169 <1079572048@qq.com>
2021-09-01 18:20:21 +0800

    style: discard useless loop, and use official scripts resources/shell for startup

--- patch/RunEasyConnect.sh
+++ patch/RunEasyConnect.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-/usr/share/sangfor/EasyConnect/resources/bin/EasyMonitor&
-/usr/share/sangfor/EasyConnect/EasyConnect --enable-transparent-visuals --disable-gpu&
-while true
-do
-    tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
-    /usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
-    sleep 2
-done
+/usr/share/sangfor/EasyConnect/resources/shell/EasyMonitor.sh&
+/usr/share/sangfor/EasyConnect/resources/shell/EasyConnect.sh&
+tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
+/usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
