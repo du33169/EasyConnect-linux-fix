503c5809464ed2fa467d22481dc4927c77e7f399
du33169 <1079572048@qq.com>
2021-09-01 19:39:47 +0800

    feat:check before launching EasyMonitor

--- patch/RunEasyConnect.sh
+++ patch/RunEasyConnect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-/usr/share/sangfor/EasyConnect/resources/shell/EasyMonitor.sh&
+checkECM=$(ps aux|grep EasyMonitor|grep -v grep)
+if [$checkECM];then
+    /usr/share/sangfor/EasyConnect/resources/shell/EasyMonitor.sh&
+fi
 /usr/share/sangfor/EasyConnect/resources/shell/EasyConnect.sh&
 tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
 /usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
