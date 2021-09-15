3424a08537a14c3a3bf9ab7abb97f26aefc5e55c
du33169 <1079572048@qq.com>
2021-09-15 20:04:45 +0800

    store key in RunEasyconnect rather than .desktop

--- patch/RunEasyConnect.sh
+++ patch/RunEasyConnect.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
-
+#!/bin/bash
 #description: this script run EasyConnect and handle error about sslservice
 #author:du33169
 
-echo "info: make sure this script is running under sudo!"
+#the sudo password
+key=
 
+if [ -z "$key" ] ; then
+    echo "fatal: sudo key is empty! Exiting..."
+    exit 1
+fi
 #check if EasyMonitor is running
 checkECM=$(ps aux|grep EasyMonitor|grep -v grep)
-if [ ! $checkECM ] ; then # not running, launch it
-    /usr/share/sangfor/EasyConnect/resources/shell/EasyMonitor.sh&
+if [ -z "$checkECM" ] ; then # not running, launch it
+    echo "EasyMonitor not running, launching it..."
+    echo ${key}|sudo -S /usr/share/sangfor/EasyConnect/resources/shell/EasyMonitor.sh start&
 fi
 
 #launch main client program 
-/usr/share/sangfor/EasyConnect/resources/shell/EasyConnect.sh&
-
+/usr/share/sangfor/EasyConnect/resources/shell/EasyConnect.sh 2>/dev/null&
 #wait for error in log
-tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
-
-#error ocurred, run the sslservice
-/usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
+echo ${key}|sudo -S tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
+#expected error ocurred, run the sslservice
+echo ${key}|sudo -S /usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
+# waiting for main program to end
+while [ -n "$(ps -e|grep EasyConnect|grep -v RunEasyConnect)" ] ; do
+        sleep 1
+done
+echo "main program stopped, exiting..."
